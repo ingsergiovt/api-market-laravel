1d76242d3289bd28c5d31a361ae371909ceb91f2
ingsergiovt <ingsergiovt@gmail.com>
2021-04-02 12:46:07 -0400

    files for payment methods model

---
+++ app/Http/Controllers/PaymentsMethodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaymentsMethod;
+use Illuminate\Http\Request;
+
+class PaymentsMethodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PaymentsMethod  $paymentsMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PaymentsMethod $paymentsMethod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PaymentsMethod  $paymentsMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PaymentsMethod $paymentsMethod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PaymentsMethod  $paymentsMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PaymentsMethod $paymentsMethod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PaymentsMethod  $paymentsMethod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PaymentsMethod $paymentsMethod)
+    {
+        //
+    }
+}
