cdde25c63ba0fc0000b1dc95c402d9dc3beffaa4
ingsergiovt <ingsergiovt@gmail.com>
2021-04-02 12:35:06 -0400

    files for dollar model

---
+++ app/Http/Controllers/DollarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dollar;
+use Illuminate\Http\Request;
+
+class DollarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Dollar  $dollar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dollar $dollar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Dollar  $dollar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Dollar $dollar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Dollar  $dollar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Dollar $dollar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Dollar  $dollar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dollar $dollar)
+    {
+        //
+    }
+}
---
+++ database/factories/DollarFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dollar;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DollarFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Dollar::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
